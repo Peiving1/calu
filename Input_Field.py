962bd0160fdb4045b92735136be8d470dc31cd89
Eivind <Eivind@gullbra.net>
2022-04-17 19:20:40 +0200

    New frame feature

--- Input_Field.py
+++ Input_Field.py
@@ -7,18 +7,23 @@ root = Tk()
 #borderwidth to change to size of the border around the entry box
 #fg and bg to change colors
 
-e = Entry(root, width=50, borderwidth=50)
-e.pack()
-e.insert(0, "Enter your name: ")
+gunnis = LabelFrame(root, text="Enter your stupid name...", padx=50, pady=50)
+gunnis.grid(padx=10, pady=10)
 
+e = Entry(gunnis, width=20, borderwidth=1)
+e.grid(row=0,column=0)
+
+e.insert(0, "Enter your name: ")
 
 def myClick():
     Hello = "Hello " + e.get() + "!"
-    myLabel = Label(root, text=Hello)
-    myLabel.pack()
+    myLabel = Label(gunnis, text=Hello)
+    myLabel.grid(row=3,column=0, columnspan=2)
+    myButton = Button(gunnis, text="Hey, " + e.get(), state=DISABLED)
+    myButton.grid(row=2, column=1)
 
 
-myButton = Button(root, text="Enter your name", command=myClick, fg="blue", bg="pink")
-myButton.pack()
+myButton = Button(gunnis, text="Enter your name", command=myClick, fg="blue", bg="pink")
+myButton.grid(row=2,column=1)
 
 root.mainloop()
